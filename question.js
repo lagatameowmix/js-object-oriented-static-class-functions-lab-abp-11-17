dd40892a44af7bb94ce426974e9e6973a4f5d69a
Sasha V <sashav4@gmail.com>
2017-11-17 05:00:22 +0000

    automatically backed up by learn

--- question.js
+++ question.js
@@ -4,6 +4,13 @@ class Question {
   return this._All;
   }
 
+//Build a static class function that can return the question by Id. 
+// function should allow you to reference the Id of the question, not its index within the _All array
+//If there are 3 questions in _All, Find(2) should return the second one, not the 3 one.
+static (Id){
+  return this.All()[Id-1]
+}
+
   constructor(content){
     this.content = content;
     this.save()
@@ -13,7 +20,5 @@ save(){
   this.constructor._All.push(this)
   }
 }
-
-
 
 Question._All = [];
