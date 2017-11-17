9c029cb09bb35c50ba8527a4d070de7658abbd21
Sasha V <sashav4@gmail.com>
2017-11-17 04:54:55 +0000

    automatically backed up by learn

--- question.js
+++ question.js
@@ -3,9 +3,9 @@ class Question {
   static All(){
   return this._All;
   }
-}
-Question._All = [];
-//Define a save() instance function that can add the instance to the _All class property.
+
 save(){
   this.constructor._All.push(this)
+  }
 }
+Question._All = [];
