0db273d9d78bf47aa275c743445be9cec5b2db26
Sasha V <sashav4@gmail.com>
2017-11-17 04:51:22 +0000

    automatically backed up by learn

--- question.js
+++ question.js
@@ -1,16 +1,10 @@
 'use strict';
-//Define a Question class using the class syntax of Javascript.
 class Question {
-  constructor(question){
-    this.question = question;
-    return this._All;
+  static All(){
+  return this._All;
   }
 }
-//Add a property, _All to the Question class and initialize its value to an empty Array.
 Question._All = [];
 //Define a class function with the static keyword, All()
 //that returns the value of the class property _All.
-class newClass{
-  static All(){
-  return this._All;
-}
+  
